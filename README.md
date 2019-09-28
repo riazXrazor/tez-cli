1d33e1e875ae836f26ad11caa56e97a62e8ed9e2
Riaz Laskar <riazcool77@gmail.com>
2019-09-28 22:08:37 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,59 @@
+tez-cli
+=======
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g tez-cli
+$ tez-cli COMMAND
+running command...
+$ tez-cli (-v|--version|version)
+tez-cli/0.0.0 win32-x64 node-v10.15.0
+$ tez-cli --help [COMMAND]
+USAGE
+  $ tez-cli COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+* [`tez-cli init [PROJECT_NAME]`]
+
+## `tez-cli init [PROJECT_NAME]`
+
+create a tez project 
+
+```
+USAGE
+  $ tez-cli init [PROJECT_NAME]
+
+OPTIONS
+  -f, --force
+  -h, --help       show CLI help
+
+EXAMPLE
+  $ tez-cli init myproject
+    creating project "myproject"... done
+    installing dependencies... done
+    Finishing setup... done
+```
+
+## `tez-cli help [COMMAND]`
+
+display help for tez-cli
+
+```
+USAGE
+  $ tez-cli help [COMMAND]
+
+ARGUMENTS
+  COMMAND  command to show help for
+
+OPTIONS
+  --all  see all commands in CLI
+```
+<!-- commandsstop -->
---
+++ src/commands/init.ts
@@ -0,0 +1,55 @@
+import {Command, flags} from '@oclif/command'
+import cli from 'cli-ux'
+// @ts-ignore
+import * as exec from 'shelljs.exec'
+// @ts-ignore
+import * as degit from 'degit'
+export default class Init extends Command {
+  static description = 'describe the command here'
+
+  static examples = [
+    `$ tez-cli init [PROJECT_NAME]`,
+  ]
+
+  static templatePath = `${__dirname}/../../template`;
+  static flags = {
+    help: flags.help({char: 'h'}),
+  }
+
+  static args = [{name: 'file'}]
+
+  async run() {
+    const {argv} = this.parse(Init)
+    const projectName = argv[0];
+    const projectPath = projectName ? `./${projectName}` : './'
+    cli.action.start(`creating project "${projectName}"`)
+
+
+    const emitter = degit('riazXrazor/ts-api-server-v2', {
+        cache: false,
+        force: true,
+        verbose: false,
+    });
+
+    try{
+        await emitter.clone(projectPath)
+        cli.action.stop()
+
+        cli.action.start(`installing dependencies`)
+        exec(`cd ${projectPath} && npm install`)
+        cli.action.stop()
+
+        cli.action.start(`Finishing setup`)
+        exec(`cd ${projectPath} && npm run gen:env`)
+        exec(`cd ${projectPath} && npm run gen:key`)
+        cli.action.stop()
+
+
+
+    } catch (e){
+      this.log(e)
+    }
+
+
+  }
+}
