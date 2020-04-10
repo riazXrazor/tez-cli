f0b02b5c16c8132897ff71a0e5280033be0bcc29
Riaz Laskar <riazcool77@gmail.com>
2020-04-10 21:51:55 +0530

    minor update

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,55 +1,78 @@
-import {Command, flags} from '@oclif/command'
-import cli from 'cli-ux'
+import { Command, flags } from "@oclif/command";
+import cli from "cli-ux";
 // @ts-ignore
-import * as exec from 'shelljs.exec'
+import * as exec from "shelljs.exec";
 // @ts-ignore
-import * as degit from 'degit'
-export default class Init extends Command {
-  static description = 'create a tez blank project'
+import * as degit from "degit";
 
-  static examples = [
-    `$ tez-cli init [PROJECT_NAME]`,
-  ]
+const boxen = require("boxen");
+const chalk = require("chalk");
+export default class Init extends Command {
+  static description = "create a tez blank project";
+
+  static examples = [`$ tez-cli init [PROJECT_NAME]`];
 
   static templatePath = `${__dirname}/../../template`;
   static flags = {
-    help: flags.help({char: 'h'}),
-  }
+    help: flags.help({ char: "h" }),
+  };
 
-  static args = [{name: 'project_name'}]
+  static args = [{ name: "project_name" }];
 
-  async run() {
-    const {argv} = this.parse(Init)
-    const projectName = argv[0];
-    const projectPath = projectName ? `./${projectName}` : './'
-    cli.action.start(`creating project "${projectName}"`)
+  notifyMsg(name: string) {
+    let installCommand;
 
+    installCommand = `cd ${name}  && npm start \n\n API Server  http://127.0.0.1:3000/ \n\n API Docs http://127.0.0.1:3000/docs`;
 
-    const emitter = degit('riazXrazor/tez', {
-        cache: false,
-        force: true,
-        verbose: false,
+    const defaultTemplate = " \nRun " + chalk.greenBright(`${installCommand}`);
+
+    const boxenOptions = {
+      padding: 1,
+      margin: 1,
+      align: "center",
+      borderColor: "yellow",
+      borderStyle: "round",
+    };
+
+    const message = "\n" + boxen(defaultTemplate, boxenOptions);
+
+    process.on("exit", () => {
+      console.error(message);
     });
 
-    try{
-        await emitter.clone(projectPath)
-        cli.action.stop()
+    process.on("SIGINT", () => {
+      console.error("");
+      process.exit();
+    });
+  }
 
-        cli.action.start(`installing dependencies`)
-        exec(`cd ${projectPath} && npm install`)
-        cli.action.stop()
+  async run() {
+    const { argv } = this.parse(Init);
+    const projectName = argv[0];
+    const projectPath = projectName ? `./${projectName}` : "./";
+    cli.action.start(`creating project "${projectName}"`);
 
-        cli.action.start(`Finishing setup`)
-        exec(`cd ${projectPath} && npm run gen:env`)
-        exec(`cd ${projectPath} && npm run gen:key`)
-        cli.action.stop()
+    const emitter = degit("riazXrazor/tez", {
+      cache: false,
+      force: true,
+      verbose: false,
+    });
 
+    try {
+      await emitter.clone(projectPath);
+      cli.action.stop();
 
+      cli.action.start(`installing dependencies`);
+      exec(`cd ${projectPath} && npm install`);
+      cli.action.stop();
 
-    } catch (e){
-      this.log(e)
+      cli.action.start(`Finishing setup`);
+      exec(`cd ${projectPath} && npm run gen:env`);
+      exec(`cd ${projectPath} && npm run gen:key`);
+      cli.action.stop();
+      this.notifyMsg(projectName);
+    } catch (e) {
+      this.log(e);
     }
-
-
   }
 }
